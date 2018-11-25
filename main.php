2ed28df27f87db3aa7ae64c0414e9c500176e741
ankitmalikg2 <stalemalik123@gmail.com>
2018-11-26 05:29:33 +0530

    first commit

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+echo "\nTest script started .........\n".
+
+include "config/main.php";
+include "validation/main.php";
+include "file_reader/main.php";
+include "curl/main.php";
+include "report_genrator/main.php";
+
+echo "\nTest script finished ........... \n";
+?>
